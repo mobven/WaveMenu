5a5d194e53cf8f054efeaf879dcb608a9d1ebce2
Ali Hasanoğlu <hasanogliali80@gmail.com>
2020-07-27 12:38:05 +0300

    Initial Commit

---
+++ WaveMenu/WaveMenu.h
@@ -0,0 +1,19 @@
+//
+//  WaveMenu.h
+//  WaveMenu
+//
+//  Created by Ali Hasanoğlu on 27.07.2020.
+//  Copyright © 2020 Ali Hasanoğlu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WaveMenu.
+FOUNDATION_EXPORT double WaveMenuVersionNumber;
+
+//! Project version string for WaveMenu.
+FOUNDATION_EXPORT const unsigned char WaveMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WaveMenu/PublicHeader.h>
+
+
