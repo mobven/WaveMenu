2668f48f80d193a2c2badf3ab52f5d630b0ec8b1
Ali Hasanoğlu <hasanogliali80@gmail.com>
2020-07-29 14:12:29 +0300

    SwiftLint issues.

--- Sources/WaveMenu/MenuTitleCell.swift
+++ Sources/WaveMenu/MenuTitleCell.swift
@@ -9,25 +9,25 @@
 import UIKit
 
 class WMTitleCell: UICollectionViewCell {
-    
+
     var titleLabelTextColor: UIColor = .black
     var titleLabelSelectedTextColor: UIColor = .white
-    
+
     // MARK: Components
     let titleLabel: UILabel = {
         let label = UILabel()
         return label
     }()
-    
+
     // MARK: Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override public func draw(_ rect: CGRect) {
         super.draw(rect)
         self.setupViews()
@@ -40,37 +40,39 @@ class WMTitleCell: UICollectionViewCell {
             _ = isSelected ? animateSelectedTitle() : setInitialSelectedTitle()
         }
     }
-    
-    func initViews(title: String){
+
+    func initViews(title: String) {
         self.titleLabel.text = title
         titleLabel.textColor = isSelected ? titleLabelSelectedTextColor : titleLabelTextColor
         _ = isSelected ? animateSelectedTitle() : setInitialSelectedTitle()
     }
-    
-    func setInitialSelectedTitle(){
+
+    func setInitialSelectedTitle() {
         titleLabel.removeFromSuperview()
-        UIView.animate(withDuration: 0.2, delay: 0.1, options: .curveEaseIn, animations: { () -> () in
+        UIView.animate(withDuration: 0.2, delay: 0.1, options: .curveEaseIn, animations: { () -> Void in
             self.setupViews()
             self.layoutIfNeeded()
         }, completion: nil)
     }
-    
-    func animateSelectedTitle(){
+
+    func animateSelectedTitle() {
         addSubview(titleLabel)
-        UIView.animate(withDuration: 0.2, delay: 0.1, options: .curveEaseIn, animations: { () -> () in
+        UIView.animate(withDuration: 0.2, delay: 0.1, options: .curveEaseIn, animations: { () -> Void in
             self.addConstraintsWithFormat("V:|-3-[v0(20)]", views: self.titleLabel)
             self.layoutIfNeeded()
         }, completion: nil)
     }
-    
+
      func setupViews() {
         addSubview(titleLabel)
         addConstraintsWithFormat("H:[v0]", views: titleLabel)
         addConstraintsWithFormat("V:[v0(20)]-20-|", views: titleLabel)
-        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: titleLabel,
+                                         attribute: .centerX,
+                                         relatedBy: .equal,
+                                         toItem: self,
+                                         attribute: .centerX,
+                                         multiplier: 1,
+                                         constant: 0))
     }
 }
-
-
-
-
